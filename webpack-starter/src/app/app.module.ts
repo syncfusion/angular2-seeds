4050f2cf9f82c9f9ce45ce71d14924f91d08f142
Abinaya Subbiah <abinaya.subbiah@syncfusion.com>
2016-10-25 13:06:40 +0530

    Revert "Components added added for routing in sample."
    This reverts commit 37d67bb02a609cef6b6716ce1392b59ac97a3cea.

--- webpack-starter/src/app/app.module.ts
+++ webpack-starter/src/app/app.module.ts
@@ -1,35 +1,16 @@
-import { NgModule, enableProdMode, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
 
-import { EJ_GRID_COMPONENTS } from './ej/grid.component';
+import {EJ_GRID_COMPONENTS} from './ej/grid.component';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { GridComponent } from './grid/grid.component';
-
-import { rootRouterConfig } from './app.routes';
-
-enableProdMode();
-
-class CustomErrorHandler implements ErrorHandler {
-  call(error, stackTrace = null, reason = null) {
-    console.log(error + "\n" + stackTrace);
-  }
-  handleError(error: any): void {
-    console.log(error);
-  }
-}
-
 @NgModule({
   imports: [
-    BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig, { useHash: true })
+    BrowserModule
   ],
   declarations: [
-    AppComponent, EJ_GRID_COMPONENTS, HomeComponent, GridComponent
+    AppComponent, EJ_GRID_COMPONENTS
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
