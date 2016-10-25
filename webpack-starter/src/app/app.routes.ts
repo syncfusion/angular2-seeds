28f55dccd9aea01227a4678bf7472ce53fa9e7db
Abinaya Subbiah <abinaya.subbiah@syncfusion.com>
2016-10-25 13:59:47 +0530

    Revert "Components added added for routing in sample."
    This reverts commit 37d67bb02a609cef6b6716ce1392b59ac97a3cea.

--- webpack-starter/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { GridComponent } from './grid/grid.component';
-import { HomeComponent } from './home/home.component';
-
-export const rootRouterConfig: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'grid', component: GridComponent }
-];
