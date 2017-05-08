422927f0fa16325d01086dbb8edf13f02def617a
Abinaya Subbiah <abinaya.subbiah@syncfusion.com>
2017-05-08 12:01:12 +0530

    Sample changes for module loading.
    Sample changes for module loading.

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,6 +3,7 @@ import {Component, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'ej-app',
   templateUrl: 'src/app.component.html',
+  styleUrls:['src/app.component.css']
 })
 export class AppComponent {
 }
